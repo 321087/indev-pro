0090f9858abbaae82de081f078139fee6162c302
Luhur Nugroho <zidanluhurnugroho@gmail.com>
2024-06-14 20:36:30 +0700

    Update

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App(){
+ return (
+  <BrowserRouter>
+  <Switch>
+    <Route exact path="/">
+      <Login/>
+    </Route>
+    <Route path="/register">
+      <Register/>
+    </Route>
+  </Switch>
+  </BrowserRouter>
+  );
+}
+
+export default App;
