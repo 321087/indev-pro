8eae1506cfa2801d8ab7f86a99f222dbb9d14c64
Luhur Nugroho <zidanluhurnugroho@gmail.com>
2024-06-20 01:04:15 +0700

    Update 19 Jun

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,39 +1,67 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confpassword, setConfPassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const history = useHistory();
+
+  const Register = async(e) =>{
+      e.preventDefault();
+      try {
+        await axios.post('http://localhost:5000/users', {
+          name: name,
+          email: email,
+          password: password,
+          confpassword: confpassword
+        });
+        history.push(" ");
+      } catch (error) {
+        if(error.response){
+          setMsg(error.response.data.msg);
+        }
+      }
+  }
+
   return (
     <section className="hero has-background-grey-light is-fullheight is-fullwidth">
       <div className="hero-body">
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-4-desktop">
-                <form className="box">
+             
+                <form onSubmit={ Register } className="box">
+                <p className='has-text-centered'>{msg}</p>
                 <div className="field mt-5">
                         <label className="label">Name</label>
                         <div className="controls">
-                            <input type="text" className="input" placeholder="Name" />
+                            <input type="text" className="input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
                         </div>
                     </div>
                     <div className="field mt-5">
                         <label className="label">Email or Username</label>
                         <div className="controls">
-                            <input type="text" className="input" placeholder="Username" />
+                            <input type="text" className="input" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                     </div>
                     <div className="field mt-5">
                         <label className="label">Password</label>
                         <div className="controls">
-                            <input type="password" className="input" placeholder="******" />
+                            <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                     </div>
                     <div className="field mt-5">
-                        <label className="label">Konfirmasi Password</label>
+                        <label className="label">Confirm Password</label>
                         <div className="controls">
-                            <input type="password" className="input" placeholder="******" />
+                            <input type="password" className="input" placeholder="******" value={confpassword} onChange={(e) => setConfPassword(e.target.value)}/>
                         </div>
                     </div>
                     <div className="field mt-5">
-                        <button className="button is-success is-fullwidth">Login</button>
+                        <button className="button is-success is-fullwidth">Register</button>
                     </div>
                 </form>
             </div>
